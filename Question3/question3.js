4aed89d50bd68ae756d12354c545f639a2a626e0
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-05 02:19:40 -0700

    Question3 added

---
+++ Question3/question3.js
@@ -0,0 +1,6 @@
+//03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+// toUpperCase : convets lowercase to uppercase.
+
+const myInfo = "my name is aman raj";
+console.log(myInfo.toUpperCase());
