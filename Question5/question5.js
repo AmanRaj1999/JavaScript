58b2d4566b14224d9dc5cb1900a0421583ca089b
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-06 08:52:29 -0700

    added

--- Question5/question5.js
+++ Question5/question5.js
@@ -1,4 +1,13 @@
 //05. Declare a varibale and assign string value to it and then split it into an array using split() method.
 
 let name = "Aman Raj";
-console.log(name.split());
+console.log(name.split()); //Expected output: [ 'Aman Raj' ]
+
+let myName = "Aman Raj";
+console.log(name.split(""));
+// Expected output:
+// [
+//     'A', 'm', 'a',
+//     'n', ' ', 'R',
+//     'a', 'j'
+//   ]
