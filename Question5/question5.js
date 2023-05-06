d71c59a183451ad645a85eea44866912056f6e49
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-06 08:54:39 -0700

    added

--- Question5/question5.js
+++ Question5/question5.js
@@ -1,5 +1,7 @@
 //05. Declare a varibale and assign string value to it and then split it into an array using split() method.
 
+//split method():This method takes a pattern and divides a String into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.(Mdn docs)
+
 let name = "Aman Raj";
 console.log(name.split()); //Expected output: [ 'Aman Raj' ]
 
