468caf8641a5c06919ea6e772db5bc31228de93d
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-07 11:06:38 -0700

    added

---
+++ Question7/question7.js
@@ -0,0 +1,6 @@
+//07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+const student =
+  "I'm a student of Pw skills.Their teachers are good.So Pw skills is the best.";
+const searchTerm = "pw skills";
+console.log(student.lastIndexOf(searchTerm)); //
