0b32ac33596ac402a3189677d73deed1ad191936
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-08 11:44:16 -0700

    added

--- Question6/question6.js
+++ Question6/question6.js
@@ -1,5 +1,7 @@
 //06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
 
+//split() :This method takes a pattern and divides a String into an ordered list of substrings by searching for the pattern, puts these substrings into an array, and returns the array.(Mdn docs)
+
 const bigTech = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
 console.log(bigTech.split(","));
 
