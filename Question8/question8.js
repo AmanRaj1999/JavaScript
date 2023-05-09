1650b167c1b2aaf9e2e45d6e3a08bfee35665896
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-09 11:29:28 -0700

    added

--- Question8/question8.js
+++ Question8/question8.js
@@ -1,1 +1,7 @@
 //08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+const sentence = "    Aman Raj    ";
+console.log(sentence);
+console.log(sentence.trim());
+
+c;
