edebcec285298f175503fa1bef31230eacbcf919
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-10 11:16:44 -0700

    added

--- Question9/question9.js
+++ Question9/question9.js
@@ -1,3 +1,5 @@
 //09. Boolean value is either true or false.
 // - Write three JavaScript statement example which provide truthy value.
 // - Write three JavaScript statement example which provide falsy value.
+
+//In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy. That is, all values are truthy except false, 0, -0, 0n, "", null, undefined, and NaN.(mdn docs)
