9f1e1766e7fad02361ebbf85b0e301b33ff02aa0
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-11 11:22:20 -0700

    added

--- Question9/question9.js
+++ Question9/question9.js
@@ -3,3 +3,9 @@
 // - Write three JavaScript statement example which provide falsy value.
 
 //In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context. All values are truthy unless they are defined as falsy. That is, all values are truthy except false, 0, -0, 0n, "", null, undefined, and NaN.(mdn docs)
+
+if (1) {
+  console.log("Hello World!");
+} else {
+  console.log("Hey everyone!");
+}
