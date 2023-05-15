df5268c5923d1d9581c564063e51bc6dcabf2cc4
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-15 12:06:36 -0700

    added

--- Question11/question11.js
+++ Question11/question11.js
@@ -12,25 +12,31 @@
 const now = new Date(); //2023-05-14T18:35:38.636Z
 console.log(now);
 
-// console.log(now.getFullYear()); //2023
+console.log(now.getFullYear()); //2023
 
 //     - What is the month today as a number?
 
-// console.log(now.getMonth()); // 4, because months are zero based or january starts from zero
+console.log(now.getMonth()); // 4, because months are zero based or january starts from zero
 
 //     - What is the day today as a number?
 
-// console.log(Number(now)); //1684089987782
+console.log(Number(now)); //1684089987782
 
 //     - What is the hours now?
 
-// console.log(now.getHours()); //11
+console.log(now.getHours()); //11
 
 //     - What is the minutes now?
 
-// console.log(now.getMinutes()); //54
+console.log(now.getMinutes()); //54
 
 //     - Find out the numbers of seconds elapsed from January 1, 1970 to now.
-const old = Date(1970, 0, 1, 0, 0);
-var diff = now.getTime() - old.getTime();
-console.log(diff);
+// const old = Date(1970, 0, 1, 0, 0);
+// var diff = now.getTime() - old.getTime();
+// console.log(diff);
+
+var prevTime = new Date(1970, 0, 1, 0, 0); // Feb 1, 2011
+var thisTime = new Date(); // now
+var diff = thisTime.getTime() - prevTime.getTime(); // now - Feb 1
+console.log(diff / (1000 * 60 * 60 * 24));
+//19492.461895625
