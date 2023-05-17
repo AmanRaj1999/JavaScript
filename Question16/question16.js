b279a304cfdb47d7a86f434a3f8a117aa0ad7851
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-17 09:46:53 -0700

    added

--- Question16/question16.js
+++ Question16/question16.js
@@ -5,3 +5,20 @@
 //     - June, July or August, the season is Summer
 
 let userInput = prompt("Enter the month: ");
+
+if (
+  (userInput == "September" && userInput == "October") ||
+  userInput == November
+) {
+  console.log("The season is Autum");
+} else if (
+  (userInput == "December" && userInput == "January") ||
+  userInput == "February"
+) {
+  console.log("The season is Winter");
+} else if (
+  (userInput == "March" && userInput == "April") ||
+  userInput == "May"
+) {
+  console.log("The season is Spring");
+}
