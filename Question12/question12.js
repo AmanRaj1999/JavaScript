29ecd767b4936c8a40383a5a94319dde5abbb8d7
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-18 04:04:44 -0700

    added

--- Question12/question12.js
+++ Question12/question12.js
@@ -3,16 +3,16 @@
 //     - DD-MM-YYYY HH:mm
 //     - DD/MM/YYYY HH:mm
 
-var date = new Date();
+let date = new Date();
 console.log(date.toLocaleDateString());
 console.log(date.toDateString());
 console.log(date.toGMTString());
 
 // format date javascript
 function dateFormater(date, separator) {
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  var year = date.getFullYear();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
   // show date and month in two digits
   if (day < 10) {
