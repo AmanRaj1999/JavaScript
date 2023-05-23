9659d75cd06ef14de61ce197d4ad145333374da5
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-23 11:59:25 -0700

    added

--- Question22/question22.js
+++ Question22/question22.js
@@ -5,3 +5,31 @@
 //     - Find the average age(all items divided by number of items)
 //     - Find the range of the ages(max minus min)
 //     - Compare the value of (min - average) and (max - average), use abs() method
+
+// - Sort the array and find the min and max age
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+ages.sort(); // Sort the array in ascending order
+const minAge = ages[0]; // Minimum age is the first element after sorting
+const maxAge = ages[ages.length - 1]; // Maximum age is the last element after sorting
+
+console.log("Minimum age:", minAge);
+console.log("Maximum age:", maxAge);
+
+// - Find the median age(one middle item or two middle items divided by two)
+let medianAge;
+const middleIndex = Math.floor(ages.length / 2);
+if (ages.length % 2 === 0) {
+  // Array has even length, take the average of the two middle values
+  medianAge = (ages[middleIndex] + ages[middleIndex - 1]) / 2;
+} else {
+  // Array has odd length, take the middle value
+  medianAge = ages[middleIndex];
+}
+
+console.log("Median age:", medianAge);
+
+//     - Find the average age(all items divided by number of items)
+const sum = ages.reduce((total, age) => total + age, 0);
+const averageAge = sum / ages.length;
+
+console.log("Average age:", averageAge);
