09e0ea175ccd4ab338db84f97d6b5f340fb3b15b
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-28 11:03:44 -0700

    added

--- Question26/question26.js
+++ Question26/question26.js
@@ -3,3 +3,11 @@
 // 	2 * 1 = 2
 // 	2 * 2 = 4
 // 	2 * 3 = 6 and so on till 2 * 10 = 20.
+// Prompt user for a number
+var number = parseInt(prompt("Enter a number:"));
+
+// Print multiplication table
+for (var i = 1; i <= 10; i++) {
+  var result = number * i;
+  console.log(number + " * " + i + " = " + result);
+}
