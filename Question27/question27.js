4857eefe85e6b755fa7484c616a09e96bc8d8164
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-29 11:04:02 -0700

    added

--- Question27/question27.js
+++ Question27/question27.js
@@ -13,3 +13,14 @@
 // 	     *
 // 	    ***
 //     *****
+
+// Pattern A: Triangle
+function printTriangle(num) {
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
