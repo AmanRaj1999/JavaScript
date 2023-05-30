7fe61632f915cbb79f335ee5b3d40f089a994954
AmanRaj1999 <amanraj241999@gmail.com>
2023-05-30 10:57:16 -0700

    added

--- Question27/question27.js
+++ Question27/question27.js
@@ -1,62 +1,20 @@
-// 28. Write a program to print the given patterns using the loops-
-// 	a. Print a triangle pattern, if the given input is 3 then the pattern should be similar to the given output
-// 	   *
-// 	   **
-// 	   ***
-
-// 	b. Print a square pattern, if the input is 3 then the output should be similar to the given output
-// 	   ***
-// 	   ***
-// 	   ***
-
-// 	c. Print a pyramid pattern, if the input is 3 then the output should be similar to the given output
-// 	     *
-// 	    ***
-//     *****
-
-// Pattern A: Triangle
-function printTriangle(num) {
-  for (let i = 1; i <= num; i++) {
-    let row = "";
-    for (let j = 1; j <= i; j++) {
-      row += "*";
-    }
-    console.log(row);
+// 27. Write a program to print all the prime number between 0 to 100 (0 and 100 included).
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
   }
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-// Example:
-printTriangle(3);
-console.log("\n");
-
-// Pattern B: Square
-function printSquare(num) {
-  for (let i = 1; i <= num; i++) {
-    let row = "";
-    for (let j = 1; j <= num; j++) {
-      row += "*";
-    }
-    console.log(row);
+for (let i = 0; i <= 100; i++) {
+  if (isPrime(i)) {
+    console.log(i);
   }
 }
-
-// Example:
-printSquare(3);
-console.log("\n");
-
-// Pattern C: Pyramid
-function printPyramidPattern(n) {
-  for (let i = 1; i <= n; i++) {
-    let row = "";
-    for (let j = 1; j <= n - i; j++) {
-      row += " ";
-    }
-    for (let k = 1; k <= 2 * i - 1; k++) {
-      row += "*";
-    }
-    console.log(row);
-  }
-}
-
-// Example usage:
-printPyramidPattern(3);
--- Question28/question28.js
+++ Question28/question28.js
@@ -1,1 +1,62 @@
+// 28. Write a program to print the given patterns using the loops-
+// 	a. Print a triangle pattern, if the given input is 3 then the pattern should be similar to the given output
+// 	   *
+// 	   **
+// 	   ***
 
+// 	b. Print a square pattern, if the input is 3 then the output should be similar to the given output
+// 	   ***
+// 	   ***
+// 	   ***
+
+// 	c. Print a pyramid pattern, if the input is 3 then the output should be similar to the given output
+// 	     *
+// 	    ***
+//     *****
+
+// Pattern A: Triangle
+function printTriangle(num) {
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+
+// Example:
+printTriangle(3);
+console.log("\n");
+
+// Pattern B: Square
+function printSquare(num) {
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+    for (let j = 1; j <= num; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+
+// Example:
+printSquare(3);
+console.log("\n");
+
+// Pattern C: Pyramid
+function printPyramidPattern(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n - i; j++) {
+      row += " ";
+    }
+    for (let k = 1; k <= 2 * i - 1; k++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+
+// Example usage:
+printPyramidPattern(3);
