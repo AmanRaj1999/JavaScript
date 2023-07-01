8e0a34402fe8018a5c7e1bca1df1cde7fefc2dba
AmanRaj1999 <amanraj241999@gmail.com>
2023-07-01 12:01:37 -0700

    Answer added

--- Question21/question21.js
+++ Question21/question21.js
@@ -1,13 +1,24 @@
 // 21. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
 
-const countries = require("./countries");
+// const countries = require("./Question19/countries");
+
+// const countryToCheck = "Ethiopia";
+
+// if (countries.includes(countryToCheck)) {
+//   console.log("ETHIOPIA");
+// } else {
+//   countries.push(countryToCheck);
+// }
+
+// console.log("Countries:", countries);
+const countries = require("../Question19/countries");
 
 const countryToCheck = "Ethiopia";
 
 if (countries.includes(countryToCheck)) {
   console.log("ETHIOPIA");
 } else {
-  countries.push(countryToCheck);
+  countries.push(...[countryToCheck]);
 }
 
 console.log("Countries:", countries);
